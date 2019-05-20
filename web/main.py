68c6c8134741db96bc1dc00d13549ce0ff492a94
SimonDanielV <simonvreugdenhil@gmail.com>
2019-05-20 22:02:19 +0200

    calculation functions implemented on app

--- web/main.py
+++ web/main.py
@@ -1,11 +1,35 @@
 #! /usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtflask.generate import Randomizer, Predictor
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/random-number", methods=['GET', 'POST'])
+def random_int():
+    if request.method == 'POST':
+        random_start = int(request.form['randomStart'])
+        random_end = int(request.form['randomEnd'])
+        random_outcome = Randomizer(random_start, random_end).make_random_number()
+        return render_template("random_number.html", random_number=random_outcome)
+    return render_template("random_number.html")
+
+@app.route("/predict", methods=['GET', 'POST'])
+def predict_int():
+    if request.method == 'POST':
+        input_numbers = [int(n.strip()) for n in request.form['numberSequence'].split(',')]
+        prediction = Predictor(input_numbers).make_prediction()
+        return render_template("predict_number.html", predicted_number=prediction)
+    return render_template("predict_number.html")
+
+# @app.route("/predict")
+# def predict_int():
+#     x = 5
+#     return render_template("predict_number.html", random_number=x)
 
 @app.route("/about")
 def about():
